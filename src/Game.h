902465244b19d290a8625ccdc9e77a063f56b200
ZeeHatcher <zee.hatcher010@gmail.com>
2019-11-20 12:05:07 +0800

    add Game class

---
+++ src/Game.h
@@ -0,0 +1,34 @@
+#pragma once
+#include <vector>
+#include "Ammo.h"
+#include "Bomb.h"
+#include "Collectible.h"
+#include "CollidableObject.h"
+#include "DrawableObject.h"
+#include "Enemy.h"
+#include "Explosion.h"
+#include "Food.h"
+#include "GameObject.h"
+#include "Menu.h"
+#include "MoveableObject.h"
+#include "Player.h"
+#include "Status.h"
+#include "Wall.h"
+
+class Game
+{
+public :
+	Game();
+	void update();
+	void draw();
+
+private :
+	Menu menu_;
+	Player* player_;
+	Status status_;
+	std::vector<Bomb*> bombs_;
+	std::vector<Collectible*> collectibles_;
+	std::vector<Enemy*> enemies_;
+	std::vector<Explosion*> explosions_;
+	std::vector<Wall*> walls_;
+};
